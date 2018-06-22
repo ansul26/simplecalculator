5ee0e9320bd9ad02557217a406b313f05ca293fb
ansul <ansul26081994@gmail.com>
2018-06-22 18:37:06 +0530

    simple calculator  and background add

--- calc.js
+++ calc.js
@@ -26,3 +26,75 @@ function setOperator(operator){
 	display.value += operator;}
 }
 
+function e(){
+
+
+	var display=document.getElementById("display");
+	if (display.value.charAt[0]=='-') {var flag=true;}
+
+	var express=display.value;
+	var exp_copy=express;
+    express = express.replace(/[0-9]+/g, "#").replace(/[\(|\|\.)]/g, "");
+    var nums = exp_copy.split(/[^0-9\.]+/);
+    var ops = express.split("#").filter(function(n){return n});
+    if (flag ==true) {
+	nums[0]= -1*parseFloat(nums[0]);
+	ops.slice(0,1);
+	for (var i = 0; i <= ops.length; i++) {
+		if (ops[i]=='/') {
+			nums[i]=parseFloat(nums[i])/parseFloat(numss[i+1]);
+			nums.splice(i+1,1);
+			ops.splice(i);
+		}
+	}
+	for (var i=0; i<=ops.length; i++){
+		if (ops[i]=='*') {
+			nums[i]=parseFloat(nums[i])*parseFloat(nums[i+1]);
+			nums.splice(i+1,1);
+			ops.splice(i);
+        }
+	}
+	for (var i=0; i<=ops.length; i++){
+		if (ops[i]=='+') {
+			nums[i]=parseFloat(nums[i])+parseFloat(nums[i+1]);
+		}
+	}
+	for (var i=0; i<=ops.length; i++){
+		if (ops[i]=='-') {
+			nums[i]=parseFloat(nums[i])-parseFloat(nums[i+1]);
+		}
+	}
+	
+
+
+}
+else {
+	for (var i = 0; i <= ops.length; i++) {
+		if (ops[i]=='/') {
+			nums[i]=parseFloat(nums[i])/parseFloat(nums[i+1]);
+			nums.splice(i+1,1);
+			ops.splice(i);
+		}
+	}
+	for (var i=0; i<=ops.length; i++){
+		if (ops[i]=='*') {
+			nums[i]=parseFloat(nums[i])*parseFloat(nums[i+1]);
+			nums.splice(i+1,1);
+			ops.splice(i);
+        }
+	}
+	for (var i=0; i<=ops.length; i++){
+		if (ops[i]=='+') {
+			nums[i]=parseFloat(nums[i])+parseFloat(nums[i+1]);
+		}
+	}
+	for (var i=0; i<=ops.length; i++){
+		if (ops[i]=='-') {
+			nums[i]=parseFloat(nums[i])-parseFloat(nums[i+1]);
+		}
+	}
+	
+}	
+display.value=Number(nums[0]);
+
+}
