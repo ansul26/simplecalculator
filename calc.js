35a3fe326a38925bfb1fa48712ee33179c28d935
ansul <ansul26081994@gmail.com>
2018-06-22 15:01:18 +0530

    changes in function name and js new without eval

--- calc.js
+++ calc.js
@@ -1,47 +1,38 @@
-displayNum="";    //initialising some variable to display num
-storedNum="";     //initialising some variable to stored num
-operation=0;      //initialising some operation to none
-queuedOperation=0; //initialising some operator to none
-calculationFinished=false; //flag to say calculation not finished
-
-// function to clear screen
 function c(){
 	var display= document.getElementById("display");
-	displayNum="";
-    storedNum="";
-    operation=0;
-    queuedOperation=0;
-    display.value=displayNum;
-
+	display.value='';
 }
 
-//function to add number
-function numInput(num){
+
+
+function numInput(digit){
 	var display= document.getElementById("display");
-	if ((display.value=="") && num==0)
-	{
-	return;
+	display.value += digit;
+
+}
+
+function addDecimal(){
+	var display= document.getElementById("display");
+	if (display.value.indexOf('.')=== -1) display.value += '.';
+}
+
+function setOperator(operator){
+	var display= document.getElementById("display");
+	if (operator == '+') {
+		display.value += '+';
+	    //document.getElementById("display") = display.value;
 	}
-	else if(calculationFinished==true){
-	display.value=num;
-	calculationFinished=false;
+	else if (operator == '-') {
+		display.value += '-';
+		//document.getElementById("display") = display.value;
 	}
-	else
-	{
-		display.value += num
+	else if (operator == '*') {
+		display.value += '*';
+		//document.getElementById("display") = display.value;
+	}
+	else if (operator == '/') {
+		display.value += '/';
+		//document.getElementById("display") = display.value;
 	}
 }
 
-//function to add decimal
-function addDecimal(dec){
-	var display= document.getElementById("display");
-	//checking if dec is already present or not if not then add
-	for (var i = 0; i < display.value.length; i++) {
-		if(display.value.charAt(i)=='.'){
-			return;
-		}
-		else{
-			display.value +=dec;
-		}
-	}
-}
