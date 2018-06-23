03a3265fa4830e46550ca90b0ec28e52bf3951e2
ansul <ansul26081994@gmail.com>
2018-06-23 23:27:44 +0530

    error shown and bug fixed for (-)

--- calc.js
+++ calc.js
@@ -28,47 +28,32 @@ function setOperator(operator){
 
 function e(){
 
+var display=document.getElementById("display");
 
-	var display=document.getElementById("display");
-	if (display.value.charAt[0]=='-') {var flag=true;}
-
+	var flag=false;
+	console.log(display.value[0]);
+	if (display.value[0]=='-') {flag=true;}
+	var bool = true;
+	opers=['/','*','+','-'];
+	for (var i = 0; i < display.value.length; i++) {
+		if (opers.indexOf(display.value[i]) != -1 && opers.indexOf(display.value[i+1]) != -1) {
+			display.value = "Error";
+			bool=false;
+		}
+	}
+	if (bool) {
 	var express=display.value;
 	var exp_copy=express;
     express = express.replace(/[0-9]+/g, "#").replace(/[\(|\|\.)]/g, "");
     var nums = exp_copy.split(/[^0-9\.]+/);
     var ops = express.split("#").filter(function(n){return n});
-    if (flag ==true) {
-	nums[0]= -1*parseFloat(nums[0]);
-	ops.slice(0,1);
-	for (var i = 0; i <= ops.length; i++) {
-		if (ops[i]=='/') {
-			nums[i]=parseFloat(nums[i])/parseFloat(numss[i+1]);
-			nums.splice(i+1,1);
-			ops.splice(i);
-		}
+    if (nums[0]==""){nums.splice(0,1);}
+    
+    if (flag==true) {
+    
+	nums[0]= (-1*parseFloat(nums[0])).toString();
+	ops.splice(0,1);
 	}
-	for (var i=0; i<=ops.length; i++){
-		if (ops[i]=='*') {
-			nums[i]=parseFloat(nums[i])*parseFloat(nums[i+1]);
-			nums.splice(i+1,1);
-			ops.splice(i);
-        }
-	}
-	for (var i=0; i<=ops.length; i++){
-		if (ops[i]=='+') {
-			nums[i]=parseFloat(nums[i])+parseFloat(nums[i+1]);
-		}
-	}
-	for (var i=0; i<=ops.length; i++){
-		if (ops[i]=='-') {
-			nums[i]=parseFloat(nums[i])-parseFloat(nums[i+1]);
-		}
-	}
-	
-
-
-}
-else {
 	for (var i = 0; i <= ops.length; i++) {
 		if (ops[i]=='/') {
 			nums[i]=parseFloat(nums[i])/parseFloat(nums[i+1]);
@@ -94,7 +79,8 @@ else {
 		}
 	}
 	
-}	
 display.value=Number(nums[0]);
+
+}
 
 }
